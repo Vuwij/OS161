2dcca0bab7bb11cb37fa1dfb1f95b4d7ca04a2a5
Jason Wang <vuwij@me.com>
2018-01-17 17:24:24 -0500

    Update

--- marker.sh
+++ marker.sh
@@ -1,13 +1,13 @@
 set path=( /cad2/ece344s/cs161/bin $path)
 
 cd root/
-os161-tester 0
+os161-tester -v 0
 #os161-tester 1
 #os161-tester 2
 #os161-tester 3
 
 #cd marker
-#os161-tester -m 0
+#os161-testPer -m 0
 #os161-tester -m 1
 #os161-tester -m 2
 #os161-tester -m 3
