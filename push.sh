f71b0b4dde5a5345708ad1d19c3914e596714b3d
Jason Wang <vuwij@me.com>
2018-01-22 04:49:12 -0500

    Commit

--- push.sh
+++ push.sh
@@ -2,11 +2,11 @@ sh pull.sh
 
 cd $HOME/Documents/ECE344/
 
+# SVN
+cd os161
+svn commit -m "Commit"
+
 # GIT
 git add .
 git commit -m "Commit"
 git push origin master
-
-# SVN
-cd os161
-svn commit -m "Commit"
