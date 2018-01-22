1abddc360b3d8979fde35f3f1ad23813fa8f8a28
Jason Wang <vuwij@me.com>
2018-01-22 04:44:57 -0500

    Commit

---
+++ pull.sh
@@ -0,0 +1,6 @@
+cd $HOME/Documents/ECE344/
+
+# GIT
+git add .
+git commit -m "Commit"
+git pull origin master
---
+++ push.sh
@@ -0,0 +1,12 @@
+sh pull.sh
+
+cd $HOME/Documents/ECE344/
+
+# GIT
+git add .
+git commit -m "Commit"
+git push origin master
+
+# SVN
+cd os161
+svn commit -m "Commit"
