96de6bfa61322ac37c5fa51d834a23aa218f225d
Jason Wang <vuwij@me.com>
2018-01-25 14:20:56 -0500

    Commit

--- push.sh
+++ push.sh
@@ -4,11 +4,14 @@ cd $HOME/Documents/ECE344/
 
 # SVN
 cd os161
+sh svn-ignore.sh
+svn add . --force
+svn status
 svn commit -m "Commit"
 cd ..
 
 # GIT
-#git add .
-#git commit -m "Commit"
-#git config --global credential.helper 'cache --timeout=10000000'
-#git push origin master
+git add .
+git commit -m "Commit"
+git config --global credential.helper 'cache --timeout=10000000'
+git push origin master
