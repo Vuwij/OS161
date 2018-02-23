56ffa74fa0682adfc25ffadf1a636a603ad0ad35
Jason Wang <vuwij@me.com>
2018-02-22 21:32:11 -0500

    Commit

--- marker.sh
+++ marker.sh
@@ -2,6 +2,6 @@ rm -rf marker/
 mkdir marker/
 sh push.sh
 cd marker/
-svn rm -m "removing assignment 0 tag" $ECE344_SVN/tags/asst0-end
-svn copy -m "ending assignment 0" $ECE344_SVN/trunk $ECE344_SVN/tags/asst0-end
+#svn rm -m "removing assignment 0 tag" $ECE344_SVN/tags/asst0-end
+svn copy -m "ending assignment 1" $ECE344_SVN/trunk $ECE344_SVN/tags/asst1-end
 os161-tester -m 1
